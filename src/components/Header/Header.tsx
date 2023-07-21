ca6e0e3dbd49d1df9d3af31f4e649f34e93b41ca
Jerrylejer <jerrylejer23@yahoo.fr>
2023-07-21 13:33:39 +0200

    HeaderComponent with responsive issues

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,11 +5,17 @@ const Header = () => {
     <header className={style.header}>
       <div>
         <h1 className={style.title}>House of Tea</h1>
-        <p>Le bar à thés d'exception</p>
+        <p className={style.subtitle}>Le bar à thés d'exception</p>
       </div>
       <div>
-        <img src="/assets/icons/cart-icon.svg" alt="Cart" />
-        <a href="/">Recommencer la commande</a>
+          <img
+            src="/assets/icons/cart-icon.svg"
+            alt="Cart"
+            className={style.cart}
+          />
+        <a href="/" className={style.redo}>
+          Recommencer la commande
+        </a>
       </div>
     </header>
   );
