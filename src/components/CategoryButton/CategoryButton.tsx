9ded958c9ae4463e33d1b6d9e90698de0200e187
diambere.cisse <diambere.cisse@outlook.com>
2023-07-21 16:40:16 +0200

    feature/create-category-button-component

---
+++ src/components/CategoryButton/CategoryButton.tsx
@@ -0,0 +1,11 @@
+import style from './CategoryButton.module.scss'
+
+const CategoryButton = () => {
+    return(
+        <button className={style.categoryBtn}>
+            Catégorie
+        </button>
+    )
+}
+
+export default CategoryButton
--- src/pages/ProductListPage/ProductListPage.tsx
+++ src/pages/ProductListPage/ProductListPage.tsx
@@ -1,9 +1,13 @@
 import ProductCard from 'components/ProductCard/ProductCard'
 import style from './ProductListPage.module.scss'
+import CategoryButton from 'components/CategoryButton/CategoryButton'
 
 const ProductListPage = () => {
     return(
         <>
+            <div>
+                <CategoryButton />
+            </div>
             <ProductCard />
         </>
     )
