aa363356d4741d04653aff4369c80eb74d82b240
Jerrylejer <jerrylejer23@yahoo.fr>
2023-07-21 10:14:27 +0200

    pour rebase

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
-import style from './Header.module.scss'
+import style from "./Header.module.scss";
 
 const Header = () => {
-    return(
-        <></>
-    )
-}
+  return (
+    <header className={style.header}>
+      <div>
+        <h1 className={style.title}>House of Tea</h1>
+        <p>Le bar à thés d'exception</p>
+      </div>
+      <div>
+        <img src="/assets/icons/cart-icon.svg" alt="Cart" />
+        <a href="/">Recommencer la commande</a>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
