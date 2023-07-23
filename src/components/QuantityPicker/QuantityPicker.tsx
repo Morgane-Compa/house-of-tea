fce388e32ea86a02545264d73d39eca3e5097a7f
Jerrylejer <jerrylejer23@yahoo.fr>
2023-07-23 18:46:25 +0200

    QuantityPicker is reusable & works

--- src/components/QuantityPicker/QuantityPicker.tsx
+++ src/components/QuantityPicker/QuantityPicker.tsx
@@ -1,11 +1,22 @@
 import style from "./QuantityPicker.module.scss";
 
-const QuantityPicker = () => {
+interface QuantityPickerProps {
+  increment: () => void;
+  totalPicker: any;
+  decrement: () => void;
+}
+
+const QuantityPicker = (props: QuantityPickerProps) => {
+  const { increment, totalPicker, decrement } = props;
   return (
     <div className={style.picker}>
-      <button className={style.decrease}>-</button>
-      <span className={style.quantity}>0</span>
-      <button className={style.increase}>+</button>
+      <button className={style.decrease} onClick={decrement}>
+        -
+      </button>
+      <span className={style.quantity}>{totalPicker}</span>
+      <button className={style.increase} onClick={increment}>
+        +
+      </button>
     </div>
   );
 };
