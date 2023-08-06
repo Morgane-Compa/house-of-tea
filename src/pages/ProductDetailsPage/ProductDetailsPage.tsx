0f9feaa8b2dd346d260765340c0bcb772a692f15
Jerrylejer <jerrylejer23@yahoo.fr>
2023-08-06 14:06:05 +0200

    Feat/ExtraList terminer le responsive

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,23 +1,24 @@
 import SizeChoiceList from "components/SizeChoiceList/SizeChoiceList";
 import style from "./ProductDetailsPage.module.scss";
-import ExtraCard from "components/ExtraCard/ExtraCard";
+// import ExtraCard from "components/ExtraCard/ExtraCard";
 import TemperatureChoiceList from "components/TemperatureChoiceList/TemperatureChoiceList";
 import IntensityChoiceList from "components/IntensityChoiceList/IntensityChoiceList";
+import ExtraList from "components/ExtraList/ExtraList";
 
 const ProductDetailsPage = () => {
 
   return (
     <>
     <SizeChoiceList />
-    {/* Créer un map sur le mock extras */}
     <TemperatureChoiceList />
     <IntensityChoiceList />
-    <div className={style.extraContainer}>
+    {/* <div className={style.extraContainer}>
     <ExtraCard name={"sugar"} image={"/assets/products/extras/citron.svg"} price={0}/>
     <ExtraCard name={"vanille"} image={"/assets/products/extras/baton-vanille.svg"} price={2}/>
     <ExtraCard name={"miel"} image={"/assets/products/extras/nids-abeilles-frais.svg"} price={4}/>
     <ExtraCard name={"baies"} image={"/assets/products/extras/baies-fraiches-forets-blanc (1).svg"} price={4}/>
-    </div>
+    </div> */}
+    <ExtraList />
     </>
   );
 };
