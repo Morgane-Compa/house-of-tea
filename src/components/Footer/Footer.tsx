ab2d8ae44d472f103fe480949c7b36ed1ab25599
diambere.cisse <diambere.cisse@outlook.com>
2023-08-18 19:27:58 +0200

    fix: path to logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         
         <footer className={style.footer}>
             <h2 className={style.title}>House of tea</h2>
-            <img src="assets\other-images\logo-hot.svg" alt="Logo de la marque HOT" className= {style.logo} />
+            <img src="/assets/other-images/logo-hot.svg" alt="Logo de la marque HOT" className= {style.logo} />
             <p className={style.slogan} >Le bar à thé d'exception</p>
         </footer>
         
