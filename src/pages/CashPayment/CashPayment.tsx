c1c7a532d93d99efefbdaa6ec62f2366d8d0d938
diambere.cisse <diambere.cisse@outlook.com>
2023-08-28 17:14:40 +0200

    feature: product-not-available

--- src/pages/CashPayment/CashPayment.tsx
+++ src/pages/CashPayment/CashPayment.tsx
@@ -1,13 +1,16 @@
 import CallToActionButton from 'components/CallToActionButton/CallToActionButton'
 import style from './CashPayment.module.scss';
 import { useNavigate } from "react-router-dom";
+import { useCartContext } from 'contextes/CartContext';
 
 const CashPayment = () => {
     const navigate = useNavigate();
+    const {getTotalCartPrice} = useCartContext();
+
     return(
         <div className={style['cash-payment']}>
             <p>Récupérez votre ticket à la prochaine étape <br /> pour régler à la caisse</p>
-            <span className={style.total}>Total: 19,60€</span>
+            <span className={style.total}>Total: {getTotalCartPrice()}€</span>
             <CallToActionButton title="Récapitulatif" buttonType='button' callback={() => {navigate('/recap')}}/>
         </div>
     )
