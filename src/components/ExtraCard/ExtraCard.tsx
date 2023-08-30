8a266193da65551360a0c07c14b2498c44537424
Jerrylejer <jerrylejer23@yahoo.fr>
2023-08-30 17:02:17 +0200

    responsive productDetailsPage + réduction des tailles et marges en desktop => à confirmer par l'équipe

--- src/components/ExtraCard/ExtraCard.tsx
+++ src/components/ExtraCard/ExtraCard.tsx
@@ -6,23 +6,23 @@ import { IExtraIngredients } from 'mocks/product.mock';
 import { IExtra } from 'pages/ProductDetailsPage/ProductDetailsPage';
 
 interface IExtraCardProps {
- extra: IExtraIngredients
- extraCallBack: (chosenExtra: IExtra) => void
+    extra: IExtraIngredients
+    extraCallBack: (chosenExtra: IExtra) => void
 }
 
 const ExtraCard = (props: IExtraCardProps) => {
     // Je déstructure mes props pour les utiliser
-    const {extra, extraCallBack} = props;
+    const { extra, extraCallBack } = props;
     // Je déstructure "extra" des props qui est de type IExtraIngredient
     // => j'ai accès à chacune des propriétés
-    const {id,  name, image, price, maxQuantity } = extra;
+    const { id, name, image, price, maxQuantity } = extra;
 
     // ************* QuantityPicker (extraCard) *****************
     const [extraQuantity, setExtraQuantity] = useState<number>(0);
     const [extraPrice, setExtraPrice] = useState<number>(0);
 
     const addExtra = () => {
-        if(extraQuantity < maxQuantity){
+        if (extraQuantity < maxQuantity) {
             //Je modifie mes states extraQ et le extraP
             const quantity = extraQuantity + 1;
             setExtraQuantity(quantity);
@@ -32,27 +32,27 @@ const ExtraCard = (props: IExtraCardProps) => {
             // lui passe les propriétés destructurées
             const extraProduct: IExtra = {
                 id: id,
-                name : name,
+                name: name,
                 initialPrice: price,
-                finalPrice : interPrice,
-                quantity : quantity
+                finalPrice: interPrice,
+                quantity: quantity
             }
             // Je passe l'objet à la callback des props
             extraCallBack(extraProduct);
         }
     }
     const removeExtra = () => {
-        if(extraQuantity >= 1){
-            const quantity = extraQuantity - 1  
+        if (extraQuantity >= 1) {
+            const quantity = extraQuantity - 1
             setExtraQuantity(quantity)
             const interPrice = price * quantity;
             setExtraPrice(interPrice)
             const extraProduct: IExtra = {
                 id,
-                name : name,
+                name: name,
                 initialPrice: price,
-                finalPrice : interPrice,                
-                quantity : quantity
+                finalPrice: interPrice,
+                quantity: quantity
             }
             extraCallBack(extraProduct);
 
@@ -72,10 +72,8 @@ const ExtraCard = (props: IExtraCardProps) => {
                     <span>{formatNumber(price)},00€</span>
                 </div>
             </div>
-            <div>
-            <QuantityPicker increment={addExtra}
-            decrement={removeExtra} totalPicker={extraQuantity} />
-            </div>
+                <QuantityPicker increment={addExtra}
+                    decrement={removeExtra} totalPicker={extraQuantity} />
         </div>
     )
 
